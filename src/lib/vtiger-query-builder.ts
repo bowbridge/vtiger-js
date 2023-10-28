b2a9210ad85fa597a7f2be0f356b07a814dccb28
Serhii Tokariev <serhii.tokariev@bowbridge.net>
2023-10-28 13:15:22 +0300

    feat: update package. Add count, limit, and, or clause

--- src/lib/vtiger-query-builder.ts
+++ src/lib/vtiger-query-builder.ts
@@ -4,18 +4,22 @@ import { VtigerFilterBuilder } from './vtiger-filter-builder';
 
 export class VtigerQueryBuilder<T> {
   constructor(
-    private moudle: StandardListType,
+    private module: StandardListType,
     private httpClient: AxiosInstance,
     private debug: boolean
   ) {}
 
-  select(fields?: Array<keyof T>): VtigerFilterBuilder<T> {
-    const query = `select ${this._handleFields(fields)} from ${this.moudle}`;
+  select(fields?: Array<keyof T> | "count(*)"): VtigerFilterBuilder<T> {
+    const query = `select ${this._handleFields(fields)} from ${this.module}`;
 
     return new VtigerFilterBuilder<T>(query, this.httpClient, this.debug);
   }
 
-  private _handleFields = (fields?: Array<keyof T>): string => {
-    return fields ? fields.join() : '*';
+  private _handleFields = (fields?: Array<keyof T> | "count(*)"): string => {
+    if (fields) {
+      return fields === "count(*)" ? fields : fields.join();
+    }
+
+    return '*';
   };
 }
